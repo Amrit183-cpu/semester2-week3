b59e0d9fb7aab4dca85f3bab678c3def345cc82d
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-02-23 21:27:24 +0000

    worksheet 2.3

--- session-2/task1/sizes.c
+++ session-2/task1/sizes.c
@@ -15,6 +15,11 @@ int main( void ) {
     printf("Char size is %ld bytes\n",sizeof(testChar));
     printf("Int size is %ld bytes\n",sizeof(testInt));
     printf("Float size is %ld bytes\n",sizeof(testFloat));
+    printf("Short int size is %ld bytes\n", sizeof(testShort));
+
+- `short int`
+- `long int`
+- `double`
 
     return 0;
 }
--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,42 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Amritpal Singh
+ * ID: 201965262
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
+    float salary = 36250; //set salary to 36250
+    float national_insurance = 8; //set NI to 8
+    float tax_rate = 15; //set tax to 15
+    float taxed_amount = 0; //var to store amount that is taxed
+    float take_home_pay = 0; //var to store final pay
+    float ni_contributions = 0; //calc ni amount
 
-    // calculate the deductions and final take-home salary
-
+    if (salary>=14000.0 && salary<=40000.0) //check salary limits
+    {
+      if (national_insurance>=0.0 && national_insurance<=10.0) //check NI limits
+      {
+         ni_contributions = salary * (national_insurance/100.0); //calc ni with percentage
+         take_home_pay = salary - ni_contributions; 
+         if (tax_rate>=10.0 && tax_rate<=30.0) //check tax limits 
+         {
+            if (take_home_pay>12500.0) //see if take home salary is still over 12500 
+            {
+            taxed_amount = (take_home_pay-12500.0) * (tax_rate/100.0); 
+            take_home_pay = take_home_pay - taxed_amount;
+            }
+         }
+      }
+    }
+    
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+    printf("Salary £%.2f \n",salary);
+    printf("NI contribution £%.2f \n",ni_contributions);
+    printf("Tax contribution £%.2f \n",taxed_amount);
+    printf("Take home salary £%.2f \n",take_home_pay);
 
     return 0;
  }
